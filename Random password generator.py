a89d25df184e947a826b91aa29bd3f4f54bbf2c3
unknown <shreyrao2004@gmail.com>
2024-02-04 05:22:44 -0800

    comment

--- Random password generator.py
+++ Random password generator.py
@@ -15,10 +15,11 @@ def generator():
     password = random.sample(all, password_length)
     passwordField.insert(0,password)
 
-def copy():
+def copy():  #used to copy the code
     random_password=passwordField.get()
     pyperclip.copy(random_password)
 
+#creating an window using tkinter
 root = Tk()
 choice=IntVar()
 root.title('****Password Generator***')
@@ -31,15 +32,16 @@ Font=('arial',13,'bold')
 lengthLabel=Label(root,text='Password Length',font=Font,bg='gray20',fg='white')
 lengthLabel.grid(pady=5)
 
-length_Box=Spinbox(root,from_=5,to_=18,width=5,font=Font)
+length_Box=Spinbox(root,from_=5,to_=18,width=5,font=Font)#to specify the lenght of the code
 length_Box.grid(pady=5)
 
 generateButton=Button(root,text='Generate',font=Font,command=generator)
 generateButton.grid(pady=5)
 
-passwordField=Entry(root,width=25,bd=2,font=Font)
+passwordField=Entry(root,width=25,bd=2,font=Font)#to get the password
 passwordField.grid()
 
+#creating a copy button
 copyButton=Button(root,text='Copy',font=Font,command=copy)
 copyButton.grid(pady=5)
 
